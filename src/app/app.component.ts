e97f4278cf0f17e1a8b1d72e75a302c699cdcbbc
Jose <pedrojcunha7@hotmail.com>
2025-02-14 10:31:54 +0100

    feat: show all movies that have titanic in title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from './services/movies.service';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'movieApi';
+export class AppComponent implements OnInit {
+  movie = "titanic";
+  constructor(private movieServices: MoviesService) { };
+
+  ngOnInit() {
+  this.movieServices.getMovies(this.movie).subscribe((data) =>{
+    console.log(data);
+  })
+  }
 }
---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+  private url = "https://api.themoviedb.org/3/search/movie";
+  private apiKey = "38f3798081f4134f4e79acc8d383a558"
+
+  private http = inject(HttpClient)
+
+  getMovies(movieName: string){
+    const params = new HttpParams()
+      .set ('query', movieName)
+      .set('api_key', this.apiKey)
+
+      return this.http.get(this.url,{params});
+  }
+
+}
